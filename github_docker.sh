7439f4d09f966588c99047fc3cbd70ed811cc10d
Sam Liu <hello@samliu.dev>
2023-06-18 00:39:28 -0400

    Update github_docker.sh

--- github_docker.sh
+++ github_docker.sh
@@ -13,7 +13,7 @@
 # The script attempts to:
 # - Clone the repository if it has not been cloned.
 # - Otherwise, pull changes from the repository.
-# - Build the code, if required, with the specified command.
+# - Build the code, if code changed, with the specified command.
 # - Run the specified command to start the app.
 #
 # To avoid the need to clone the repository multiple times, it is advised to
@@ -22,13 +22,18 @@
 #
 # Usage
 # ==============================================================================
-# bash <(curl -s )
-# 
-# Environment variables required:
+# 1. Set up environment variables:
 # - RUN_COMMAND   - Required - The command to run the code
 # - BUILD_COMMAND - Required - The command to build the code
-# - GIT_REPO      - Required - The repo to deploy (e.g. someuser/somerepo)
+# - GIT_REPO      - Required - The repo to deploy (e.g. https://github.com/someuser/somerepo.git)
 # - GIT_BRANCH    - Required - The branch to deploy (e.g. master)
 # - GITHUB_TOKEN  - Optional - A personal access token if the repo is private
 # - GITHUB_USER   - Optional - The username associated with the token
+# 2. (Optional) Create a writable volume mapped to the working directory.
+# 3. Setup docker to run the following command per startup.
+#   bash <(curl -s https://sh.samliu.dev/github_docker.sh)
+#
+# ==============================================================================
+
+echo "Preparing to Deploy Repository ${GIT_REPO}..."
 
